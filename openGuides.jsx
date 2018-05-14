f3755c6f3b7cafb2feb97f19887a0be3651e8f80
I'm a noob <thongthaibm@gmail.com>
2018-05-15 06:11:40 +0700

    Uploads some scripts I created

---
+++ openGuides.jsx
@@ -0,0 +1,25 @@
+﻿main();
+
+function main() {
+     if(app.documents.length <= 0) {
+    	return false;
+     }
+    
+    f = new File;
+    f.changePath(Folder.desktop);
+    file_obj = f.openDlg("Open guides", "Text file:*.txt");
+    if(file_obj != null) {
+        file_obj.open('r')
+        while(!file_obj.eof) {
+            line = file_obj.readln();
+            parts = line.split(',');
+            if(parts[0] == 'Direction.HORIZONTAL')
+                activeDocument.guides.add(Direction.HORIZONTAL, parts[1]);
+            else
+                activeDocument.guides.add(Direction.VERTICAL, parts[1]);            
+        }
+        file_obj.close()
+    }
+    f.close()
+    alert('Loaded guides from file!');
+}
---
+++ saveGuides.jsx
@@ -0,0 +1,19 @@
+﻿main();
+
+function main() {
+     if(app.documents.length <= 0) {
+    	return false;
+     }
+ 
+    f = new File;
+    f.changePath(Folder.desktop);
+    file_obj = f.saveDlg("Save guides", "Text file:*.txt");
+    if(file_obj != null) {
+        file_obj.open('e')
+        for(i = 0; i < activeDocument.guides.length; i++) {
+            file_obj.writeln(activeDocument.guides[i].direction + ',' + activeDocument.guides[i].coordinate);
+        }
+        file_obj.close()
+    }
+    f.close()
+}
